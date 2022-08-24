9a9bbb44ee194cc81425ee91ee4d2cf9505f13e0
maxneedspats <benzimm07@gmail.com>
2022-08-24 11:57:50 -0500

    Add debug mode

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-import csv
 import os
 import wget
-import pandas as pd
 
 print('Welcome to the SimCo analyzer!')
 
-# Downloads new prices
-print('Downloading market price information...')
 file_name = 'exchange_info.csv'
-# if file_name in os.listdir():  # Removes old versions of exchange_info.csv
-#    os.remove('exchange_info.csv')
-# wget.download('https://docs.google.com/spreadsheets/d/e/2PACX-1vTqF15cr_qWfAjNL-zp1IWH7RM_T-\
-# xudXewWO5IkNwpvBFYZHrglDFYsdumH2EduNgysIFm2oB3g95n/pub?gid=1547132983&single=true&output=csv', file_name)
-print('Download finished!')
+debug = False
+download = True
+
+# Downloads new prices
+if download:
+    print('Downloading market price information...')
+    if file_name in os.listdir():  # Removes old versions of exchange_info.csv
+        os.remove('exchange_info.csv')
+    wget.download('https://docs.google.com/spreadsheets/d/e/2PACX-1vTqF15cr_qWfAjNL-zp1IWH7RM_T-\
+    xudXewWO5IkNwpvBFYZHrglDFYsdumH2EduNgysIFm2oB3g95n/pub?gid=1547132983&single=true&output=csv', file_name)
+    print('Download finished!')
 
 # I just read the csv manually, I couldn't figure out how to use pandas, and csv skipped the last week-ish.
 with open(file_name) as file:
@@ -26,4 +28,5 @@ prices = {}
 for i in range(len(names_list)):
     prices[names_list[i]] = prices_list[i]
 
-
+if debug:
+    print(prices)
