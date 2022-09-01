56a139f7eb1f842fd1ab0ccaf10729122e09fa04
askiiart <benzimm.07@gmail.com>
2022-09-01 11:36:02 -0500

    Update to ignore .tsv files

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from map_manager import MapManager
 print('Welcome to the SimCo analyzer!')
 
 map_slots = 10
-download_new = True
+download_new = False
 debug = False
 
 manager = MapManager(map_slots, debug, download_new, max_building_cost=10_000_000)
