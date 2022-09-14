52eef90231c11138bca3925ef65344b357cbdef2
askiiart <benzimm.07@gmail.com>
2022-09-14 15:49:34 -0500

    Clean up

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from map_manager import MapManager
 
 print('Welcome to the SimCo analyzer!')
 
-map_slots = 10
-download_new = False
-debug = False
+map_slots = 12
+download = True
+debug = True
+max_building_cost = 100_000_000
 
-manager = MapManager(map_slots, debug, download_new, max_building_cost=10_000_000)
-manager.run()
-print(manager.get_results())
+manager = MapManager(map_slots, debug, download, max_building_cost)
+#manager.run()
+#print(manager.get_results())
