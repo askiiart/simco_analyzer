63760087e3f04d14a56d7b0b2b8962c0d94f8898
askiiart <benzimm07@gmail.com>
2022-09-23 14:13:18 -0500

    Clean up better

--- main.py
+++ main.py
@@ -28,14 +28,13 @@ map_slots = 12
 download = True
 max_building_cost = 100_000_000
 
-# Turns on debug if the user ID contains "debug"
-debug = configcat_client.get_value('debug', False, user)
 if debug:
     print('Debug:', debug)
     print('Map slots:', map_slots)
     print('Download:', download)
     print('Max building cost', max_building_cost)
 
+# Run
 manager = MapManager(map_slots, max_building_cost, download_new=download, debug=debug)
 #manager.run()
 #print(manager.get_results())
